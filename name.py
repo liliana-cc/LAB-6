31c86f3f683e3e4bd8d14fdbc89d1e2d035d900f
Pamela Vishka <pamelavishka@pamelas-macbook-air.local>
2023-10-31 23:10:32 -0400

    pamela's portion

--- name.py
+++ name.py
@@ -14,16 +14,31 @@ def encode(password):
 
   return pwEncoded
 
+def decode(password): #this function decodes the password that was previously encoded
+  original = ""
+  for i in password:
+    num = int(i) - 3
+    if num == (-1):
+      num = 9
+    elif num == (-2):
+      num == 8
+    elif num == (-3):
+      num = 7
+      original += str(num)
+    return original
+
 if __name__ == "__main__":
 
-  print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
-  option = int(input('Please enter an option: '))      # user input
-  while option != 3:
-    if option == 1: # encode password
-      password = input("Please enter your password to encode: ")
-      encodedPW = encode(password)
-      print('Your password has been encoded and stored!')
-    if option == 2:
-      pass
-    if option == 3: #quit
-      break
+  while True:
+    print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n') #menu that is printed out
+    option = int(input('Please enter an option: '))      # user input
+    if option != 3:
+          if option == 1: # encode password
+            password = input("Please enter your password to encode: ")
+            encodedPW = encode(password)
+            print('Your password has been encoded and stored!')
+          if option == 2:
+            original = decode(encodedPW)
+            print(f'The encoded password is {encodedPW}, and the original password is {password}')
+          if option == 3: #quit
+            break
